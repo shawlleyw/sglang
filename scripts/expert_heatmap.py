ad5d5c4c9da22d1a4c5fc00e143ff711997d79f7
shaoyuw <wangshao@usc.edu>
2025-04-10 21:08:57 +0000

    generate heatmap for decode steps

--- scripts/expert_heatmap.py
+++ scripts/expert_heatmap.py
@@ -55,15 +55,23 @@ def main():
     n = nlayers_per_step * nsteps
     
     batch_sizes = [] # n * nranks
+    is_decode = [] # n * nranks
     ntokens_per_expert = [] # n * nexperts
     attn_elapse = []
     moe_elapse = []
     attn_all_gather_elapse = []
     all_gather_elapse = []
     
+    decode_steps = []
+    
     for i in range(nsteps):
         
         batch_sizes.append([metrics_all_ranks[k][i].batch_size for k in range(nranks)])
+        is_decode.append([metrics_all_ranks[k][i].is_decode for k in range(nranks)])
+        
+        step_is_decode = all(is_decode)
+        if step_is_decode:
+            decode_steps.append(i)
         
         for j in range(nlayers_per_step):
             ntokens = []
@@ -79,28 +87,35 @@ def main():
                 attn_all_gather.append(metric.attention_elapse[j] + metric.all_gather_elapse[j])
                 all_gather.append(metric.all_gather_elapse[j])
                 
-                
             ntokens_per_expert.append(ntokens)
             attn_elapse.append(attn)
             moe_elapse.append(moe)
             all_gather_elapse.append(all_gather)
             attn_all_gather_elapse.append(attn_all_gather)
-    
+            
+    print(f"decode steps: {decode_steps}")
+            
     expert_num_tokens = np.array(ntokens_per_expert).reshape((nsteps, nlayers_per_step, -1))
     moe_elapse = np.array(moe_elapse).reshape((nsteps, nlayers_per_step, -1))
     global_expert_num_tokens = expert_num_tokens.sum(axis=0) / 1000
     
+    decode_expert_num_tokens = expert_num_tokens[decode_steps]
+    decode_moe_elapse = moe_elapse[decode_steps]
+    decode_global_expert_num_tokens = decode_expert_num_tokens.sum(axis=0) / 1000
+    
     def get_path(filename):
         return os.path.join(directory, filename)
     
+    def get_decode_path(filename):
+        return os.path.join(directory, f"decode_{filename}")
+    
     heatmap(global_expert_num_tokens, "global #tokens per expert", "#thousand tokens", get_path("global_expert_num_tokens.png"))
-    
     heatmap(expert_num_tokens[100], "one batch #tokens per expert", "#tokens", get_path("one_batch_expert_num_tokens.png"))
-    
     heatmap(moe_elapse[100], "one batch execution time per expert", "time cost(ms)", get_path("moe_elapse.png"))
     
-
+    heatmap(decode_global_expert_num_tokens, "decode #tokens per expert", "#thousand tokens", get_decode_path("decode_global_expert_num_tokens.png"))
+    heatmap(decode_expert_num_tokens[50], "decode one batch #tokens per expert", "#tokens", get_decode_path("decode_one_batch_expert_num_tokens.png"))
+    heatmap(decode_moe_elapse[50], "decode one batch execution time per expert", "time cost(ms)", get_decode_path("decode_moe_elapse.png"))
     
-            
 if __name__ == '__main__':
     main()
