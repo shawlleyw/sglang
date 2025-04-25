d1cad69032844aa648644d0af434134f61d004c6
shaoyuw <wangshao@usc.edu>
2025-04-25 05:19:01 +0000

    refine run.sh

--- run.sh
+++ run.sh
@@ -1,31 +1,35 @@
 #!/usr/bin/bash
 
+
+METRICS_DIR=gqa_top2
 TOPK=2
 ATTN=gqa
+PREFIX="$ATTN"_top"$TOPK"
 
 for rate in 100 200 300 400 500; do
     echo "Running with request rate: $rate"
-    target_dir=short_top"$TOPK"_"$ATTN"/rate"$rate"
+    target_dir="$PREFIX"_short/rate"$rate"
     mkdir -p $target_dir
     num_reqs=$(($rate * 100))
     python -m sglang.bench_serving --dataset-name random --random-input-len "(30, 70)" --random-output-len "(70, 130)" --num-prompts $num_reqs --metrics --disable-stream --request-rate=$rate
-    mv metrics_dir/* $target_dir
+    mv $METRICS_DIR/* $target_dir
 done
 
-for rate in 40 60 80 120 160 200; do
+for rate in 50 100 150 200 250; do
     echo "Running with request rate: $rate"
-    target_dir=resonable_top"$TOPK"_"$ATTN"/rate"$rate"
-    mkdir -p $target_dir
-    num_reqs=$(($rate * 100))
-    python -m sglang.bench_serving --dataset-name random --random-input-len "(100, 300)" --random-output-len "(100, 500)" --num-prompts $num_reqs --metrics --disable-stream --request-rate=$rate
-    mv metrics_dir/* $target_dir
-done
-
-for rate in 50 100 200 300 400; do
-    echo "Running with request rate: $rate"
-    target_dir=resonable_v2_top"$TOPK"_"$ATTN"/rate"$rate"
+    target_dir="$PREFIX"_medium/rate"$rate"
     mkdir -p $target_dir
     num_reqs=$(($rate * 100))
     python -m sglang.bench_serving --dataset-name random --random-input-len "(50, 150)" --random-output-len "(50, 250)" --num-prompts $num_reqs --metrics --disable-stream --request-rate=$rate
-    mv metrics_dir/* $target_dir
+    mv $METRICS_DIR/* $target_dir
 done
+
+for rate in 25 50 75 100; do
+    echo "Running with request rate: $rate"
+    target_dir="$PREFIX"_reasonable/rate"$rate"
+    mkdir -p $target_dir
+    num_reqs=$(($rate * 100))
+    python -m sglang.bench_serving --dataset-name random --random-input-len "(100, 300)" --random-output-len "(100, 500)" --num-prompts $num_reqs --metrics --disable-stream --request-rate=$rate
+    mv $METRICS_DIR/* $target_dir
+done
+
